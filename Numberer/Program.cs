0e86837d54d71b3aff32c58dadf440f587b9180f
krismoegokstad <kristian.moe@gokstadakademiet.no>
2024-01-11 11:24:42 +0100

    jeg sier hei på deg

---
+++ Numberer/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Numberer;
+
+class Program // hei på deg
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("numberer");
+        foreach (string s in args)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
